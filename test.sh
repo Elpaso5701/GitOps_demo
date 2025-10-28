44644abd50a09deb833225821e41b6636725564c
Oleksii <tentacool5701@gmail.com>
2025-10-28 09:54:59 +0000

    move k8s to root

---
+++ test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Адрес твоего EventListener (замени на свой внешний адрес или localhost:8080 при port-forward)
+WEBHOOK_URL="http://localhost:8080"
+
+# Секрет, который ты указал в Tekton (если нужен HMAC, для простоты можно не использовать)
+GITHUB_SECRET="mygithubsecret"
+
+# Пример payload (можно изменить под свой TriggerBinding)
+read -r -d '' PAYLOAD << EOM
+{
+  "ref": "refs/heads/main",
+  "repository": {
+    "clone_url": "https://github.com/Elpaso5701/GitOps_demo.git",
+    "full_name": "Elpaso5701/GitOps_demo",
+    "name": "gitops_demo"
+  },
+  "head_commit": {
+    "id": "f31de586e8b4aab24ed8adccda3b93d13465bac1"
+  }
+}
+EOM
+
+# Отправка запроса (без подписи)
+curl -X POST "$WEBHOOK_URL" \
+  -H "Content-Type: application/json" \
+  -H "X-GitHub-Event: push" \
+  -H "X-Hub-Signature: sha1=demo" \
+  -d "$PAYLOAD"
+
+echo
