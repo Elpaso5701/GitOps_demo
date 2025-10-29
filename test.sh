0692b3c7cb22276e3e92df3ac6d0f54b9959e7af
Oleksii Shvets <tentacool5701@gmail.com>
2025-10-29 13:10:15 +0000

    m

--- test.sh
+++ test.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Адрес твоего EventListener (замени на свой внешний адрес или localhost:8080 при port-forward)
+
 WEBHOOK_URL="http://localhost:8080"
 
-# Секрет, который ты указал в Tekton (если нужен HMAC, для простоты можно не использовать)
 GITHUB_SECRET="mygithubsecret"
 
-# Пример payload (можно изменить под свой TriggerBinding)
+
 read -r -d '' PAYLOAD << EOM
 {
   "ref": "refs/heads/main",
@@ -16,12 +15,11 @@ read -r -d '' PAYLOAD << EOM
     "name": "gitops_demo"
   },
   "head_commit": {
-    "id": "d61aea02dbe0f3184f25b16a8f0efcdc039854b8"
+    "id": "f1eb375fab713ac25576f1899c37ceece7154461"
   }
 }
 EOM
 
-# Отправка запроса (без подписи)
 curl -X POST "$WEBHOOK_URL" \
   -H "Content-Type: application/json" \
   -H "X-GitHub-Event: push" \
