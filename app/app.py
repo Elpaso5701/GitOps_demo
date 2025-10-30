1f7a35698f180a8b9738c69573c54802acc33771
Oleksii Shvest <tentacool5701@gmail.com>
2025-10-30 12:27:21 +0000

    m

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask Web Server 10:38!"
+    return "Hello from Flask Web Server 14:27!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
